9c0c7dad75108fcb94f2dd131f93471105adc3d3
Priyanka <Priyanka@LAPTOP-VNPRV150>
2020-08-19 20:37:27 +0530

    first commit

---
+++ Happyshopping/src/test/java/shopping/shoppy.java
@@ -0,0 +1,22 @@
+package shopping;
+
+import org.testng.annotations.Test;
+
+public class shoppy {
+	@Test
+	public void test1()
+	{
+		System.out.println("first test");
+	}
+	@Test
+	public void test2()
+	{
+		System.out.println("second test");
+	}
+	@Test
+	public void test3()
+	{
+		System.out.println("third test");
+	}
+
+}
